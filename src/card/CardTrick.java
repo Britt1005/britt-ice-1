f13e298e8fcc1950cb15a687bf6e87a879b9ebaa
Britt1005 <rathorbr@sheridancollege.ca>
2023-05-29 14:02:43 -0400

    Added new branch

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -2,14 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package card;
 
+package card;
+import java.util.*;
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
+@author rathorbr
  */
+
 public class CardTrick {
     
     public static void main(String[] args)
@@ -18,14 +17,67 @@ public class CardTrick {
         
         for (int i=0; i<magicHand.length; i++)
         {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            Card A = new Card();
+           
+          A.setValue((int)(Math.random() * 13 + 1));
+           
+          A.setSuit(Card.SUITS[(int)(Math.random() *3+0)]);
+        
+            magicHand[i]=A;
+        }
+
+        Scanner br=  new Scanner(System.in);
+        System.out.println("Please enter a number from 1 to 13");
+        int entered_num = br.nextInt();
+        while(entered_num <1 || entered_num>13){
+                    System.out.println("Wrong Input, Please enter a number from 1 to 13");
+                    entered_num = br.nextInt();
+        }
+
+        System.out.println("Please enter the suit from Clubs, Spades, Diamonds, Hearts");
+        String str = br.next();
+        
+        while( str.equals("Hearts")==false && str.equals("Clubs")==false && str.equals("Spades")==false && str.equals("Diamonds")==false ){
+            System.out.println("Wrong Input, Please enter a correct Suit");
+                    str = br.next();
         }
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-    }
+       int ans = 0;
+
+       System.out.println("The Values set by System are as follows : \n");
+for(int i = 0;i<magicHand.length; i++){
+    System.out.println("Value = "+magicHand[i].getValue()+"\tSuit = "+magicHand[i].getSuit() );
+}
     
+    System.out.println("\n\nYour Selections are Value: "+entered_num + "\tSuit = "+str+"\n");
+     
+        for(int i=0; i<magicHand.length;i++)
+        {
+            if((magicHand[i].getValue()==entered_num) && (magicHand[i].getSuit().equals(str))){
+                ans = 1;
+                break;
+            }      
+        }
+        
+       if(ans==1) System.out.println("Oh yeah, You Won\n ");
+       else System.out.println("Oops No Match, Try again\n");
+        br.close();
+        
+       Card A = new Card();
+        A.setValue(7);
+        A.setSuit("Hearts");
+        int ans2=0;
+          for(int i=0; i<magicHand.length;i++)
+        {
+            if((magicHand[i].getValue()==A.getValue()) && (magicHand[i].getSuit().equals(A.getSuit()))){
+                ans2 = 1;
+                break;
+            }      
+        }
+        
+        System.out.println("Lucky Card:\n\t Value = "+ A.getValue() + "\n\tSuit = "+ A.getSuit()+"\n");
+        System.out.print("Did the Lucky Card Match : \t");
+        if(ans2==1) System.out.println("Yes\n");
+        else System.out.println("No\n");  
+    }   
 }
